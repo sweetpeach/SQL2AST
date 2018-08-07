e805a0d0d5a3715e563917f57c1ed1ff10e729c7
Shirley Anugrah Hayati <sweetpineappleforest@gmail.com>
2018-08-07 17:30:48 -0400

    add command to run with WikiSQL dataset

---
+++ codegen2/finalproject/wikisql_train.sh
@@ -0,0 +1,35 @@
+output="../../files/runs"
+device="cpu"
+
+# sql dataset
+echo "training wikisql dataset"
+dataset="wikisql_dataset.bin"
+commandline="-batch_size 10 -max_epoch 50 -valid_per_batch 4000 -save_per_batch 4000 -decode_max_time_step 750 -optimizer adadelta -rule_embed_dim 128 -node_embed_dim 64 -valid_metric bleu -no_retrieval"
+datatype="sql"
+
+# train the model
+THEANO_FLAGS="mode=FAST_RUN,device=${device},floatX=float32" python2 -u code_gen.py \
+	-data_type ${datatype} \
+	-data ../../files/${dataset} \
+	-output_dir ${output} \
+	${commandline} \
+	train
+
+# decode testing set, and evaluate the model which achieves the best bleu and accuracy, resp.
+for model in "model.best_bleu.npz" "model.best_acc.npz"; do
+	THEANO_FLAGS="mode=FAST_RUN,device=${device},floatX=float32" python2 code_gen.py \
+	-data_type ${datatype} \
+	-data ../../files/${dataset} \
+	-output_dir ${output} \
+	-model ${output}/${model} \
+	${commandline} \
+	decode \
+	-saveto ${output}/${model}.decode_results.test.bin
+
+	python2 code_gen.py \
+		-data_type ${datatype} \
+		-data ../../files/${dataset} \
+		-output_dir ${output} \
+		evaluate \
+		-input ${output}/${model}.decode_results.test.bin
+done
